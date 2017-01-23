eeb0e7c0c5a2fbf0975d458d5b4a0f259b2adeb8
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-01-23 15:31:25 +0200

    added the create action to add an object form a form.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,42 @@
 class ProductsController < ApplicationController
+	def index
+		@products = Product.all
+	end
+
+	def new
+	end
+
+	def create
+		name = params[:name]
+		description = params[:description]
+		image = params[:image]
+
+		@product = Product.new({name: name, description: description, image: image})
+
+		@product.save
+
+
+		if @product.save
+			redirect_to @product
+		else
+			render 'new'
+		end	
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
 end
