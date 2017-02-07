b0820ac141f4900c20561b2e45839e925490ddce
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-02-07 09:48:33 +0200

    Added associations for products and categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ApplicationRecord
 	belongs_to :supplier
 	has_many :images
 	has_many :orders
-	
+	has_many :category_products
+	has_many :categories, through: :category_products
+
 
 	def self.discounted_products
 		@products = Product.where("price < ", 600000)		
