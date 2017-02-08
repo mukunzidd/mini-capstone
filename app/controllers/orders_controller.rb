af9fc5e3d35b818bd211923436d65a05b9f393cc
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-02-08 16:16:37 +0200

    Added a shopping cart for each unique user.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,15 +3,15 @@ class OrdersController < ApplicationController
     @order = Order.find_by(id: params[:id])
   end
 
-  def update
-  	@order = Order.find_by(id: params[:id])
-  	quantity = params[:quantity]
-	  total = @order.subtotal + @order.tax
+  # def update
+  # 	@order = Order.find_by(id: params[:id])
+  # 	quantity = params[:quantity]
+	 #  total = @order.subtotal + @order.tax
 
-  	@order = Order.find_by(id: params[:id])
+  # 	@order = Order.find_by(id: params[:id])
 
-  	@order = Order.update(total: total, open: true, user_id: current_user.id)
+  # 	@order = Order.update(total: total, open: true, user_id: current_user.id)
 
-  	render :show  	
-  end
+  # 	render :show  	
+  # end
 end
